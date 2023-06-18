53cfb62b6044a0709c6146f2831d37a64b55889a
Ahmed-kotb621 <akotb621@gmail.com>
2023-06-18 14:53:43 +0300

    add files

---
+++ main.js
@@ -0,0 +1,55 @@
+// Output 
+// window.alert("Welcome to js");
+// document.write("<h1>Welcome to java file</h1>");
+// console.log("Welcome to java file");
+
+//////////////////////////////////////////////////////////
+// template literals (String literals)
+/*
+let a = "Ahmed";
+let b = "Sayed";
+
+console.log(`Welcome ${a} ${b}`);
+*/
+////////////////////////////////////////////////////
+/*
+let cardTitle ="Elzero",cardDesc="Elzero Web School",cardDate ="25/10";
+
+let card = `<div class="card">
+    <h3>Hello ${cardTitle}</h3>
+    <p>${cardDesc}</p>
+    <span>${cardDate}</span>
+</div>
+`
+
+
+document.write(card.repeat(4));   
+*/
+/*
+// Number methode 
+console.log(("100").toString());
+console.log(100.10.toString());
+console.log((100.101222).toFixed(4));
+console.log(parseInt(1000.20));
+console.log(parseInt('100 ahmed'));
+console.log(parseFloat(1000.20));
+console.log(Number.isInteger(100));
+*/
+
+// Math Object
+/*
+console.log(Math.round(10.5));
+console.log(Math.floor(10.5));
+console.log(Math.ceil(10.5));
+console.log(Math.random());
+console.log(Math.min(10,5,6,22,7));
+console.log(Math.max(10,5,6,22,7));
+console.log(Math.trunc(22.5));
+console.log(Math.pow(2, 5));
+*/
+
+
+
+
+
+ 
